f6fdd80a0c2b101d8c98bcf9cae8d2b12b2d8432
gregory-houy <houygregory2009@gmail.com>
2025-04-10 18:36:32 +0200

    Correction de l'URL du backend pour Vercel sur le fichier script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function() {
     errorMessage.textContent = '';
 
     try {
-      const response = await fetch('https://page-de-connexion-html-cfjuszhkc-mdl-lptcbs-projects.vercel.app//api/login', {
+      const response = await fetch('https://page-de-connexion-html.vercel.app/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
