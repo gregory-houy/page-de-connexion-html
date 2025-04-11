c733ad577258120b49255c1ad59f17331fe2f30e
gregory-houy <houygregory2009@gmail.com>
2025-04-11 08:27:24 +0200

    Changements de quelques fichiers

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const loginForm = document.getElementById('loginForm');
-  const errorMessage = document.getElementById('errorMessage');
+  const loginForm = document.getElementById('loginForm');
+  const errorMessage = document.getElementById('errorMessage');
 
-  loginForm.addEventListener('submit', async function(event) {
-    event.preventDefault();
+  loginForm.addEventListener('submit', async function(event) {
+    event.preventDefault();
 
-    const usernameInput = document.getElementById('username');
-    const passwordInput = document.getElementById('password');
-    const username = usernameInput.value.trim();
-    const password = passwordInput.value.trim();
+    const usernameInput = document.getElementById('username');
+    const passwordInput = document.getElementById('password');
+    const username = usernameInput.value.trim();
+    const password = passwordInput.value.trim();
 
-    errorMessage.textContent = '';
+    errorMessage.textContent = '';
 
-    try {
-      const response = await fetch('https://page-de-connexion-html.vercel.app/api/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username, password }),
-      });
+    try {
+      const response = await fetch('https://page-de-connexion-html.vercel.app/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
 
-      const data = await response.json();
+      const data = await response.json();
 
-      if (response.ok) {
-        localStorage.setItem('username', username);
-        window.location.href = 'page_accueil.html';
-      } else {
-        errorMessage.textContent = data.error || 'Erreur de connexion.';
-      }
-    } catch (error) {
-      console.error('Erreur lors de la requête de connexion:', error);
-      errorMessage.textContent = 'Erreur de connexion.';
-    }
-  });
+      if (response.ok) {
+        localStorage.setItem('username', username);
+        window.location.href = 'page_accueil.html';
+      } else {
+        errorMessage.textContent = data.error || 'Erreur de connexion.';
+      }
+    } catch (error) {
+      console.error('Erreur lors de la requête de connexion:', error);
+      errorMessage.textContent = 'Erreur de connexion.';
+    }
+  });
 });
